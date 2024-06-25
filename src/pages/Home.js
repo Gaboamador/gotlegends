da9047c9b3d3a7ba8f03adffa29bb72067d4c938
Gaboamador <gaboamador@hotmail.com>
2024-06-25 17:54:19 -0300

    Agregado Popover Overlay en pantalla de inicio

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect, useRef } from "react";
 import '../App.css';
-import {Container, Image, Table, Button, ButtonGroup, ButtonToolbar } from 'react-bootstrap';
+import {Container, Image, Table, Button, ButtonGroup, ButtonToolbar, Overlay, OverlayTrigger, Popover } from 'react-bootstrap';
 import Context from "../context";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link, useNavigate } from 'react-router-dom';
@@ -21,7 +21,6 @@ const Home = () => {
   const context = useContext(Context)
   const navigate=useNavigate()
   const refs = useRef({});
-
 
   const [builds, setBuilds] = useState([]);
   
@@ -70,6 +69,30 @@ const Home = () => {
     return itemType ? itemType.toLowerCase() : '';
   };
 
+  const getTechniquesArray = (className) => {
+    switch (className) {
+      case 'Assassin':
+        return items.assassinTec;
+      case 'Hunter':
+        return items.hunterTec;
+      case 'Ronin':
+        return items.roninTec;
+      case 'Samurai':
+        return items.samuraiTec;
+      default:
+        return [];
+    }
+  };
+  
+  // Helper function to find the position of a perk in its tier
+  const getPerkPosition = (className, perkName, tier) => {
+    const techniques = getTechniquesArray(className);
+    const tierTechniques = techniques.filter(technique => technique.slot === tier);
+    const position = tierTechniques.findIndex(technique => technique.name === perkName) + 1;
+    return position;
+  };
+  
+
   const handleHeaderClick = (buildClass, buildName) => {
     context.setSelectedBuild(buildName)
     context.setSelectedClass(buildClass)
@@ -83,6 +106,48 @@ const Home = () => {
     }
   };
 
+  const getPopoverContent = (className, build, gearType) => {
+    const gear = groupedBuilds[className]?.find(b => b.name === build.name);
+    if (!gear) return null;
+  
+    const gearProps = {
+      name: gear[gearType],
+      prop1: gear[`${gearType}_prop1`],
+      prop2: gear[`${gearType}_prop2`],
+      perk1: gear[`${gearType}_perk1`],
+      perk2: gear[`${gearType}_perk2`],
+    };
+
+    const getPerkDescription = (perk) => {
+      const perkItem = getTechniquesArray(className)?.find(item => item.name === perk);
+      return perkItem ? perkItem.description : perk;
+    };
+    
+  
+    return (
+      <Popover id="popover-basic" className={`${getItemTypeClassName(gearType, gear[gearType])}`}>
+        {(gearType === "katana" || gearType === "ranged" || gearType === "charm" || gearType === "gw1" || gearType === "gw2") ? (
+        <>
+        <Popover.Header className={`${getItemTypeClassName(gearType, gear[gearType])} popover-build-header`}>{gearProps.name}</Popover.Header>
+        <Popover.Body className="popover-build-details">
+          {gearProps.prop1 && <div className="not-perk">{`${gearProps.prop1}`}</div>}
+          {gearProps.prop2 && <div className="not-perk">{`${gearProps.prop2}`}</div>}
+          {gearProps.perk1 && <div className="perk">{`${gearProps.perk1.toUpperCase()}`}</div>}
+          {gearProps.perk2 && <div className="perk">{`${gearProps.perk2.toUpperCase()}`}</div>}
+        </Popover.Body>
+        </>
+        ):(
+        <>
+          <Popover.Header className="popover-build-header techniques">{gearProps.name}</Popover.Header>
+          <Popover.Body className="popover-build-details">
+          {gearType && <div className="not-perk">{`${getPerkDescription(build[gearType])}`}</div>}
+          </Popover.Body>
+        </>
+          )}
+      </Popover>
+    );
+  };
+  
 
   return (
     <div className="content">
@@ -132,9 +197,22 @@ const Home = () => {
                     <tr className="clickable-header-buildName" onClick={() => handleHeaderClick(className, build.name)}>
                       <td colSpan="5" className="build-name">{build.name.toUpperCase()}</td>
                     </tr>
+                    <tr className="subheader-buildName">
+                      <td colSpan="2" className="build-name-sub left">{build.ability}</td>
+                      <td className="build-name-sub">
+                        {getPerkPosition(className, build.perk1, 'Perk 1')}
+                      </td>
+                      <td className="build-name-sub">
+                        {getPerkPosition(className, build.perk2, 'Perk 2')}
+                      </td>
+                      <td className="build-name-sub rigth">
+                        {getPerkPosition(className, build.perk3, 'Perk 3')}
+                      </td>
+                    </tr>
                     <tr className="build-tec-row">
                     <td colSpan="5">
                       <div className="build-tec-row-container">
+                      <OverlayTrigger trigger="click" placement="bottom" overlay={getPopoverContent(className, build, 'ultimate')} rootClose>
                           <div className="icon-container-round">
                           <img
                                 src={techniquesToImage[build.ultimate]}
@@ -142,17 +220,21 @@ const Home = () => {
                                 className="build-image-selected"
                               />
                               </div>
+                              </OverlayTrigger>
                               {/* </td>
                             <td> */}
+                            <OverlayTrigger trigger="click" placement="bottom" overlay={getPopoverContent(className, build, 'ability')} rootClose>
                             <div className="icon-container-round">
                               <img
                                 src={techniquesToImage[build.ability]}
-                                alt={build.ability}
+                                alt={build.ultimate}
                                 className="build-image-selected"
                               />
                               </div>
+                              </OverlayTrigger>
                             {/* </td>
                             <td> */}
+                            <OverlayTrigger trigger="click" placement="bottom" overlay={getPopoverContent(className, build, 'perk1')} rootClose>
                               <div className="icon-container">
                               <img
                                 src={techniquesToImage[build.perk1]}
@@ -160,8 +242,10 @@ const Home = () => {
                                 className="build-image-selected"
                               />
                               </div>
+                              </OverlayTrigger>
                             {/* </td>
                             <td> */}
+                            <OverlayTrigger trigger="click" placement="bottom" overlay={getPopoverContent(className, build, 'perk2')} rootClose>
                             <div className="icon-container">
                               <img
                                 src={techniquesToImage[build.perk2]}
@@ -169,8 +253,10 @@ const Home = () => {
                                 className="build-image-selected"
                               />
                               </div>
+                              </OverlayTrigger>
                             {/* </td>
                             <td> */}
+                            <OverlayTrigger trigger="click" placement="bottom" overlay={getPopoverContent(className, build, 'perk3')} rootClose>
                             <div className="icon-container">
                               <img
                                 src={techniquesToImage[build.perk3]}
@@ -178,45 +264,56 @@ const Home = () => {
                                 className="build-image-selected"
                               />
                               </div>
+                              </OverlayTrigger>
                           </div>
                       </td>
                     </tr>
                     <tr className="build-gear-row">
-                      <td className={getItemTypeClassName('katana', build.katana)}>
+                    <OverlayTrigger trigger="click" placement="bottom" overlay={getPopoverContent(className, build, 'katana')} rootClose>
+                      <td className={`pointer ${getItemTypeClassName('katana', build.katana)}`}>
                         <img
                           src={techniquesToImage[build.katana]}
                           alt={build.katana || 'No Katana'}
                           className="build-image"
                         />
                       </td>
-                      <td className={getItemTypeClassName('ranged', build.ranged)}>
+                      </OverlayTrigger>
+                      <OverlayTrigger trigger="click" placement="bottom" overlay={getPopoverContent(className, build, 'ranged')} rootClose>
+                      <td className={`pointer ${getItemTypeClassName('ranged', build.ranged)}`}>
                         <img
                           src={techniquesToImage[build.ranged]}
                           alt={build.ranged || 'No Ranged'}
                           className="build-image"
                         />
                       </td>
-                      <td className={getItemTypeClassName('charm', build.charm)}>
+                      </OverlayTrigger>
+                      <OverlayTrigger trigger="click" placement="bottom" overlay={getPopoverContent(className, build, 'charm')} rootClose>
+                      <td className={`pointer ${getItemTypeClassName('charm', build.charm)}`}>
                         <img
                           src={techniquesToImage[build.charm]}
                           alt={build.charm || 'No Charm'}
                           className="build-image"
                         />
                       </td>
-                      <td className={getItemTypeClassName('gw1', build.gw1)}>
+                      </OverlayTrigger>
+                      <OverlayTrigger trigger="click" placement="bottom" overlay={getPopoverContent(className, build, 'gw1')} rootClose>
+                      <td className={`pointer ${getItemTypeClassName('gw1', build.gw1)}`}>
                         <img
                           src={techniquesToImage[build.gw1]}
                           alt={build.gw1 || 'No GW1'}
                           className="build-image"
                         />
                       </td>
-                      <td className={getItemTypeClassName('gw2', build.gw2)}>
+                      </OverlayTrigger>
+                      <OverlayTrigger trigger="click" placement="bottom" overlay={getPopoverContent(className, build, 'gw2')} rootClose>
+                      <td className={`pointer ${getItemTypeClassName('gw2', build.gw2)}`}>
                         <img
                           src={techniquesToImage[build.gw2]}
                           alt={build.gw2 || 'No GW2'}
                           className="build-image"
                         />
                       </td>
+                      </OverlayTrigger>
                     </tr>
                   </React.Fragment>
                 ))}
